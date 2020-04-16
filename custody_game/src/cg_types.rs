f46cd52a756135845ed7be8f8acedc7f62ab31d8
Valdas Rakutis <valdas@rakutis.lt>
2020-04-16 12:40:31 +0300

    Custody Game: add basic types and constants

---
+++ custody_game/src/cg_types.rs
@@ -0,0 +1,38 @@
+use bls::{Signature};
+use types::primitives::{Epoch, ValidatorIndex};
+use types::types::Attestation;
+
+#[derive(Clone, Copy)]
+pub struct CustodySlashing{
+    //Attestation.custody_bits_blocks[data_index][committee.index(malefactor_index)] is the target custody bit to check.
+    pub data_index: u64,
+    pub malefactor_index: ValidatorIndex,
+    //pub malefactor_secret: Signature,
+    pub whistleblower_index: ValidatorIndex,
+    //(Attestation.data.shard_transition_root as ShardTransition).shard_data_roots[data_index] is the root of the data.
+    //pub shard_transition: ShardTransition TODO: can't find this, prob eth2.0 thing
+    //pub attestation: Attestation,
+    //pub data: ByteList[MAX_SHARD_BLOCK_SIZE], probably byte[size]
+}
+
+pub struct SignedCustodySlashing{
+    pub message: CustodySlashing,
+    pub signature: Signature
+}
+
+pub struct CustodyKeyReveal{
+    //index of the validator whose key is being revealed
+    pub revealer_index: ValidatorIndex,
+    //Masked signature
+    pub reveal: Signature
+}
+
+pub struct EarlyDerivedSecretReveal{
+    pub revealed_index: ValidatorIndex,
+    pub epoch: Epoch,
+    pub reveal: Signature,
+    pub masker_index: ValidatorIndex,
+    //pub mask: Bytes32
+}
+
+
---
+++ custody_game/src/main.rs
@@ -0,0 +1,10 @@
+mod cg_types;
+
+fn main() {
+    let slashing = cg_types::CustodySlashing{
+        data_index: 0,
+        malefactor_index: 0,
+        whistleblower_index: 0
+    };
+    println!("Well this was a success");
+}
